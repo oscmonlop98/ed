8c716d06d23b00b0fdef1da8979f3b7801a05751
oscmonlop98 <oscarmorenolopinfo2@gmail.com>
2018-10-18 12:16:22 +0200

    Añadido JVector: propuesto indexOf

---
+++ JVector/src/org/institutoserpis/ed/Vector.java
@@ -0,0 +1,16 @@
+package org.institutoserpis.ed;
+
+public class Vector {
+	
+	public static void main(String[] args) {
+		int[] v = new int[] {16, 12, 15, 14, 17};
+		int x = 14;
+		int index = indexOf(v, 14);
+		System.out.println("index=" + index);
+	}
+	
+	public static int indexOf (int[] v, int x) {
+		return 3;
+	}
+
+}
