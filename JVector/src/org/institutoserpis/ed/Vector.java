ffdc6d2d55b1347f8518eedfba07096e3492ff3b
oscmonlop98 <oscarmorenolopinfo2@gmail.com>
2018-11-19 09:40:42 +0100

    Actualizado .gitignore

--- JVector/src/org/institutoserpis/ed/Vector.java
+++ JVector/src/org/institutoserpis/ed/Vector.java
@@ -1,19 +1,19 @@
 package org.institutoserpis.ed;
 
 public class Vector {
-	
+
 	public static void main(String[] args) {
+		// TODO Auto-generated method stub
 		int[] v = new int[] {16, 12, 15, 14, 17};
-//		for (int item : v)
-//			System.out.println(item);
-//		
-//		int index = indexOf(v, 12);
-//		System.out.println("index=" + index);
-		int minimo = min(v);
-		System.out.println("El menor es " + minimo);
+		for (int item : v)
+			System.out.println(item);
+		
+		int index = indexOf(v, 12);
+		System.out.println("index="+index);
 	}
+
 	
-	public static int indexOf (int[] v, int x) {
+	public static int indexOf(int[] v, int x) {
 //		int index = 0;
 //		while (index < v.length && v[index] != x)
 //			index++;
@@ -26,14 +26,35 @@ public class Vector {
 		return -1;
 	}
 	
-	public static int min (int[] v) {
-		int min= v[0];
-		for (int index = 0; index < v.length; index++)
-		if (min > v[index])
-			index++;
-		else
-			min = v[index];			
-		return min; 
+	public static int min(int[] v) {
+//		int min = v[0];
+//		for (int index = 1; index < v.length; index++)
+//			if (v[index] < min)
+//				min = v[index];
+//		return min;
+		int indexOfMin = indexOfMin(v);
+		return v[indexOfMin]; 
 	}
-
+	
+	public static int indexOfMin(int[] v) {
+		int indexOfMin = 0;
+		for (int index = 1; index < v.length; index++)
+			if (v[index] < v[indexOfMin])
+				indexOfMin = index;
+		return indexOfMin;
+	}
+	
+	public static void sort(int[] v) {
+		//TODO implementar mediante selección directa
+		int aux = 0, indexOfMin = 0;
+		
+		for (indexOfMin = 0; indexOfMin < v.length - 1; indexOfMin++) {
+		for (int index = 1; index < v.length; index++)
+			if (v[index] < v[indexOfMin])
+				indexOfMin = index;
+		aux = v[0];
+		v[0] = v[indexOfMin];
+		v[indexOfMin] = aux;
+		}
+	}
 }
--- JVector/src/org/institutoserpis/ed/VectorTest.java
+++ JVector/src/org/institutoserpis/ed/VectorTest.java
@@ -7,16 +7,14 @@ import org.junit.jupiter.api.Test;
 class VectorTest {
 
 	@Test
-	void IndexOf() {
+	void indexOf() {
 		int[] v = new int[] {16, 12, 15, 14, 17};
 		assertEquals(0, Vector.indexOf(v, 16));
-		assertEquals(1, Vector.indexOf(v, 12));
 		assertEquals(2, Vector.indexOf(v, 15));
-		assertEquals(3, Vector.indexOf(v, 14));
 		assertEquals(4, Vector.indexOf(v, 17));
 		assertEquals(-1, Vector.indexOf(v, 33));
 	}
-
+	
 	@Test
 	void indexOfEmpty() {
 		int[] v = new int[] {};
@@ -24,8 +22,33 @@ class VectorTest {
 	}
 	
 	@Test
-	void min () {
+	void min() {
 		assertEquals(12, Vector.min(new int[] {16, 12, 15, 14, 17}));
+		assertEquals(9, Vector.min(new int[] {9, 12, 15, 14, 17}));
+		assertEquals(7, Vector.min(new int[] {16, 12, 15, 14, 7}));
+		assertEquals(57, Vector.min(new int[] {57}));
+		assertEquals(57, Vector.min(new int[] {57, 98}));
+		assertEquals(57, Vector.min(new int[] {98, 57}));
+	}
+	
+	@Test
+	void indexOfMin() {
+		assertEquals(1, Vector.indexOfMin(new int[] {16, 12, 15, 14, 17}));
+		assertEquals(0, Vector.indexOfMin(new int[] {9, 12, 15, 14, 17}));
+		assertEquals(4, Vector.indexOfMin(new int[] {16, 12, 15, 14, 7}));
+		assertEquals(0, Vector.indexOfMin(new int[] {57}));
+		assertEquals(0, Vector.indexOfMin(new int[] {57, 98}));
+		assertEquals(1, Vector.indexOfMin(new int[] {98, 57}));
 	}
 
+	@Test
+	void sort() {
+		int[] v = new int[] {15, 12};
+		Vector.sort(v);
+		assertArrayEquals(new int[] {12, 15}, v);
+		v = new int[] {17, 12, 15};
+		Vector.sort(v);
+		assertArrayEquals(new int[] {12, 15, 17}, v);
+		
+	}
 }
