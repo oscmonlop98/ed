47e2ba04440db444af09939970c66872359600b8
oscmonlop98 <oscarmorenolopinfo2@gmail.com>
2019-02-04 08:57:37 +0100

    Añadido CGtkTable

---
+++ CGtkTable/CGtkTable/MainWindow.cs
@@ -0,0 +1,41 @@
+﻿using System;
+using Gtk;
+
+public partial class MainWindow : Gtk.Window
+{
+    private string jugador = "X";
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+        Table table = new Table(3, 3, true);
+        for (int row = 0; row < 3; row++)
+            for (int column = 0; column < 3; column++) {
+                Button button = new Button();
+                table.Attach(button, (uint)column, (uint)column + 1, (uint)row, (uint)row + 1);
+                button.Clicked += delegate {
+                    if (button.Label != null)
+                        return;
+                    button.Label = jugador;
+                    if (jugador == "X")
+                        jugador = "0";
+                    else
+                        jugador = "X";
+                };
+            }
+        vBox.Add(table);
+        table.ShowAll();
+
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+
+    //protected void OnButtonClicked(object sender, EventArgs e)
+    //{
+    //  ((Button)sender).Label = "X";
+    //  //button_el_que_sea.Label = "X";
+    //}
+}
