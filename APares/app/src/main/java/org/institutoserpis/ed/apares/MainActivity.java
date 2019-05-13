6d3cd6a8fdb27897e32e9af57f0e65d07dc541bd
oscmonlop98 <oscarmorenolopinfo2@gmail.com>
2019-05-13 08:53:22 +0200

    Actualizado

--- APares/app/src/main/java/org/institutoserpis/ed/apares/MainActivity.java
+++ APares/app/src/main/java/org/institutoserpis/ed/apares/MainActivity.java
@@ -1,15 +1,19 @@
 package org.institutoserpis.ed.apares;
 
+import android.os.Handler;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
 import android.widget.Button;
 
+import java.util.ArrayList;
 import java.util.Arrays;
+import java.util.Collections;
+import java.util.List;
 
 public class MainActivity extends AppCompatActivity {
-    private Button button, button2, buton3, button4, button5, button6, button7, button8, button9, button10, button11, button12, buttonPlay, buttonPower;
+    /*private Button button, button2, buton3, button4, button5, button6, button7, button8, button9, button10, button11, button12, buttonPlay, buttonPower;
 
     String palabras [];
     Button [] botonera = new Button [12];
@@ -34,14 +38,20 @@ public class MainActivity extends AppCompatActivity {
     public void onButtonClick2(View view) {
         Button button2 = (Button) view;
         button2.setText("DOS");
-    }
+    }*/
 
-    /*private List<String> palabras = new ArrayList<>(
+    private List<String> palabras = new ArrayList<>(
             Arrays.asList("En", "un", "lugar", "de")
     );
     private List<Integer> buttonIds = Arrays.asList(
-            R.id.button, R.id.button2
+            R.id.button1, R.id.button2, R.id.button3, R.id.button4,
+            R.id.button5, R.id.button6, R.id.button7, R.id.button8
     );
+    private boolean esPrimeraPalabra = true;
+    private String primeraPalabra = "";
+    private Button primerButton = null;
+    private Handler handler = new Handler();
+
     private void initPalabras() {
         palabras.addAll(palabras);
         Collections.shuffle(palabras);
@@ -51,18 +61,34 @@ public class MainActivity extends AppCompatActivity {
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
-        initPalabras();
+        initPalabras();}
 
-        private void onButtonClick(View view) {
+    private void onButtonClick(View view) {
         int index = buttonIds.indexOf(view.getId());
+        String palabra = palabras.get(index);
         Button button = (Button)view;
-        button.setText(palabras.get(index));
+        button.setText(palabra);
+        if (esPrimeraPalabra) {
+            primeraPalabra = palabra;
+            primerButton = button;
+            esPrimeraPalabra = false;
+
+        }else {
+            esPrimeraPalabra = true;
         }
-    }*/
+            if (!palabra.equals(primeraPalabra))
+                handler.postDelayed(() -> {
+                    primerButton.setText("");
+                    button.setText("");
+                }, 4000
+                );
+        }
+    }
 
 
 
 
 
 
-}
+
+
