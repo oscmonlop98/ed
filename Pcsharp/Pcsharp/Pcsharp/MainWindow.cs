ebb4c5f546aa7f7da776b8ce29d65cca517def21
oscmonlop98 <oscarmorenolopinfo2@gmail.com>
2019-05-16 12:22:25 +0200

    Pcsharp implementando editAction

---
+++ Pcsharp/Pcsharp/Pcsharp/MainWindow.cs
@@ -0,0 +1,44 @@
+﻿using System;
+using Gtk;
+
+public partial class MainWindow : Gtk.Window
+{
+    public MainWindow() : base(Gtk.WindowType.Toplevel)
+    {
+        Build();
+		treeView.AppendColumn("Nombre", new CellRendererText(), "text", 0);
+		treeView.AppendColumn("Teléfono", new CellRendererText(), "text", 1);
+		ListStore listStore = new ListStore(typeof(String), typeof(String));
+		treeView.Model = listStore;
+		listStore.AppendValues("Rosa", "777666555");
+		listStore.AppendValues("Pepe", "777666554");
+		listStore.AppendValues("Ana", "777666553");
+
+		editAction.Sensitive = false;
+
+		newAction.Activated += (sender, e) => {
+			vBoxContacto.Visible = true;
+			treeView.Sensitive = false;
+		};
+
+        editAction.Activated += (sender, e) => {
+            vBoxContacto.Visible = true;
+            treeView.Sensitive = false;
+            treeView.Selection.GetSelected(out TreeIter treeIter);
+            string nombre = (string)treeView.Model.GetValue(treeIter, 0);
+            string telefono = (string)treeView.Model.GetValue(treeIter, 1);
+            Console.WriteLine("Nombre={0} Teléfono={1}", nombre, telefono);
+            entryNombre.Text = nombre;
+            entryTelefono.Text = telefono;
+        };
+
+        treeView.Selection.Changed += (sender, e) => 
+            editAction.Sensitive = treeView.Selection.CountSelectedRows() > 0;
+    }
+
+    protected void OnDeleteEvent(object sender, DeleteEventArgs a)
+    {
+        Application.Quit();
+        a.RetVal = true;
+    }
+}
